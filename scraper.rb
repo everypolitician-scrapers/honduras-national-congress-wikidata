2537cd053127874bf22e8b02b280a572fab2ca29
Tony Bowden <tony@tmtm.com>
2016-02-19 09:21:00 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,32 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
-require 'rest-client'
 
-def members
-  morph_api_url = 'https://api.morph.io/tmtmtmtm/honduras-national-congress-wikipedia/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikiname) AS wikiname from data"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/honduras-national-congress-wikipedia', column: 'wikiname')
 
-WikiData.ids_from_pages('es', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('es') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+EveryPolitician::Wikidata.scrape_wikidata(names: { es: names }, output: false)
+warn EveryPolitician::Wikidata.notify_rebuilder
 
